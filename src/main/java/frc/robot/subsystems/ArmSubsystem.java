eb365b8951c516bf168198c6cc83f28473bac841
Stevethegeek529 <125223140+Stevethegeek529@users.noreply.github.com>
2025-03-12 19:02:17 -0400

    new arm values

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -28,7 +28,7 @@ public class ArmSubsystem extends SubsystemBase {
     double position = m_armEncoder.getPosition();
 
     //ensure the arm does not go below the upper or lower limit
-    if ((position <= 0.025 && speed < 0) /*upper limit */ || (position >= 0.3 && speed > 0)) /*lower limit */ {
+    if ((position <= 0.185 && speed < 0) /*upper limit */ || (position >= 0.48 && speed > 0)) /*lower limit */ {
       armMotor.set(0); //stop the motor if it tries to go past the limits
     } else {
       speed *= 0.5; //reduce the speed of the arm motor
